b3d82700448a3ce10b5ab10dcb9451ba9c392335
Miguel Daipré <miguel.daipre@totvs.com.br>
2024-09-18 19:44:51 -0300

    feat: add initial nitro location package

---
+++ packages/react-native-nitro-location/ios/NitroLocationOnLoad.mm
@@ -0,0 +1,23 @@
+//
+//  NitroLocationOnLoad.mm
+//  NitroLocation
+//
+//  Created by Marc Rousavy on 22.07.24.
+//
+
+#import <Foundation/Foundation.h>
+#import <NitroModules/HybridObjectRegistry.hpp>
+
+@interface NitroLocationOnLoad : NSObject
+@end
+
+@implementation NitroLocationOnLoad
+
+using namespace margelo::nitro;
+
++ (void)load {
+  // TODO: Register all Swift Hybrid Objects here
+  // TODO: Register all C++ Hybrid Objects here
+}
+
+@end
