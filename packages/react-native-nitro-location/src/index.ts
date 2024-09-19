d201b7abe58b363971f93a38b31575b15d3ed1ca
Miguel Daipré <miguel.daipre@totvs.com.br>
2024-09-18 21:09:45 -0300

    feat: export NitroLocation module

--- packages/react-native-nitro-location/src/index.ts
+++ packages/react-native-nitro-location/src/index.ts
@@ -1,1 +1,12 @@
-// TODO: Export all HybridObjects here
+import { NitroModules } from 'react-native-nitro-modules'
+import type { Location } from './specs/Location.nitro'
+
+export * from './specs/Location.nitro'
+export * from './specs/LocationCoords.nitro'
+export * from './specs/LocationResult.nitro'
+
+/**
+ * Export NitroLocation to expose all methods
+ */
+export const NitroLocation =
+  NitroModules.createHybridObject<Location>('LocationFactory')
